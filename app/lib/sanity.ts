c3cfef320519592b337c03b19c58b847ead6117a
alyza <alyzamae003@gmail.com>
2023-11-01 15:11:19 +0800

    first-commit

---
+++ app/lib/sanity.ts
@@ -0,0 +1,13 @@
+import { createClient } from "next-sanity"
+
+const projectId = 'your-project-id'
+const dataset = 'production'
+const apiVersion = '2023-01-01'
+
+export const client = createClient({
+    projectId,
+    dataset,
+    apiVersion,
+    useCdn: true,
+});
+
